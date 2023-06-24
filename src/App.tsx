1d6eddaed2029b6c1470a6da550ba755fec8b804
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-06-24 18:40:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { COLORS, Color } from "./types/Color";
 import { useMemo, useState } from "react";
 
+import ChooseLevels from "./components/ChooseLevels";
+import { Color } from "./types/Color";
 import GameButtons from "./components/GameButtons";
 import GameDirections from "./components/GameDirections";
 import Header from "./components/Header";
+import useHandleColorClick from "./hooks/useHandleColorClick";
+import useHandlePlay from "./hooks/useHandlePlay";
 
 function App() {
 
@@ -13,46 +16,34 @@ function App() {
   const [time, setTime] = useState<number>(0);
   const [gameColors, setGameColors] = useState<Color[]>([])
   const correctColor = useMemo<Color>(() => gameColors.find((color) => color.correct)!, [gameColors])
-
-  const handlePlay = () => {
-    setStatus("playing");
-    setTime(0);
-    setScore(0);
-    setMoves(10)
-
-    const [correctColor, wrongColor] = COLORS.slice().sort(()=> Math.random()-0.5)
-    setGameColors([{...correctColor, correct: true}, {...wrongColor, correct: false}].sort(() => Math.random() - 0.5))
-  }
-
-
-  const handleColorClick = (clickedColor: Color) => {
-    setMoves((moves) => moves - 1)
-    if(clickedColor.correct){
-      setScore((score)=> score + 1);
-      if(moves === 1){
-        setStatus("finished")
-      } else {
-        const [correctColor, wrongColor] = COLORS.slice().sort(()=> Math.random()-0.5)
-        setGameColors([{...correctColor, correct: true}, {...wrongColor, correct: false}].sort(() => Math.random() - 0.5))
-      }
-    } else {
-      setScore((score)=> score - 1);
-      if(moves === 1){
-        setStatus("finished")
-      }else {
-        const [correctColor, wrongColor] = COLORS.slice().sort(()=> Math.random()-0.5)
-        setGameColors([{...correctColor, correct: true}, {...wrongColor, correct: false}].sort(() => Math.random() - 0.5))
-      }
-      
+  const {handleColorClick} = useHandleColorClick({setMoves,moves,setScore,score,setStatus,setGameColors}) 
+  //const {handlePlayEasy} = useHandlePlay({setTime, setMoves, setScore, setStatus, setGameColors})
+/*
+  const initialState = () => ({
+    score: 0
+  })
+  const reducer = (state = initialState(), action = {}) => {
+    switch(action.type){
+      case 'INCREMENT':
+        return {score: state.score + 1}
+      case 'DECREMENT':
+        return {score: state.score - 1}
+      case 'RESET':
+        return {score: 0}
+      default:
+        return state;
     }
   }
+
+  const [state, dispatch] = useReducer(reducer, reducer())
+  const incrementScore = ()=> dispatch({type: 'INCREMENT'})*/
 
   return (
     <main>
       <Header score={score} status={status} time={time} setTime={setTime} />
       {status === 'playing' && (<GameDirections  moves={moves} gameColors={gameColors} correctColor={correctColor} />)}
       <footer>
-        {status === 'initial' && <button onClick={handlePlay}>Play</button>}
+        {status === 'initial' && <ChooseLevels setTime={setTime}  setMoves={setMoves} setScore={setScore} setStatus={setStatus} setGameColors={setGameColors} />}
         {status === 'finished' && <button onClick={()=> setStatus('initial')}>Start Over</button>}
         {status === 'playing' && gameColors && <GameButtons handleColorClick={handleColorClick} gameColors={gameColors} />}
       </footer>
